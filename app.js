c9ecfc317abe41b664eb83a9ac2bcc706bef32c0
Logan Sinclair <rocket3701@yahoo.com>
2018-01-24 19:34:58 -0600

    Fix images, adds more pages

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ app.use(express.static('res'));
 app.set("view engine", "ejs");
 app.disable('x-powered-by');
 
-app.get("/home", (req, res)=>{
-  res.render("pages/home");
+app.get("/projects", (req, res)=>{
+  res.render("pages/projects");
 })
 
-app.get("/projects/", (req, res)=>{
-  res.render("pages/projects");
+app.get("/about", (req, res)=>{
+  res.render("pages/about");
+})
+
+app.get("/", (req, res)=>{
+	res.render("pages/home");
+})
+
+app.get("/*", (req, res)=>{
+  res.render("pages/404");
 })
 
 app.listen(8080, function(){
