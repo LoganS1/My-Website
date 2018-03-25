7890b439c5edd3c4ab1024affaa5461065f2a9e7
LoganS1 <rocket3701@yahoo.com>
2018-03-25 14:35:42 -0500

    Add auto updating with Github Webhooks + Add favicon

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 var express = require("express"),
-	app 			= express(),
-  ejs       = require("ejs");
-
+	app 	= express(),
+	https	= require("https"),
+	ejs     = require("ejs"),
+	crypto	= require("crypto");
+	  
 app.use(express.static('res'));
 app.set("view engine", "ejs");
 app.disable('x-powered-by');
 
 app.get("/", (req, res)=>{
+	console.log(req.url);
 	res.render("pages/home");
 })
 
 app.get("/projects*", (req, res)=>{
+	console.log(req.url);
 	res.sendFile(__dirname + req.url, (err)=>{
 		if(err){
-			res.render("pages/404");
+			res.status(404).render("pages/404");
 		}
 	});
 })
 
-app.get("/*", (req, res)=>{
-	res.render("pages/404");
+app.post("/update", (req, res)=>{
 	console.log(req.url);
+	let key = process.env.GithubSecretKey;
+	let mySignature = crypto.createHmac("sha1", key).update(JSON.stringify(webhookBody)).digest("hex");
+	let githubSignature = req.headers["x-hub-signature"];
+	if(mySignature === githubSignature){
+		console.log("Authenticated Github Webhook... Updating Server & Projects");
+		res.status(202).send("Pulling updates from Github for Website & Projects!");
+		require('child_process').exec(update.sh);
+	}else{
+		console.log("Invalid attempt to update server! + Signature given: " + githubSignature);
+		res.status(401).send("Invalid Signature!");
+	}
+})
+
+app.get("/*", (req, res)=>{
+	console.log(req.url);
+	res.status(404).render("pages/404");
 })
 
 app.listen(8080, function(){
---
+++ update.sh
@@ -0,0 +1,12 @@
+#Go into Snake-Game directory and update
+cd projects/Snake-Game
+git pull origin master
+#Back out of directory
+cd ../
+#Go into Langstons-Ant directory and update
+cd Langstons-Ant
+git pull origin master
+#back into main directory
+cd ../../
+#update main website
+git pull origin master
